c133c21251005e4c2a114cd85b793aa26aba7472
alizmq02 <alizmq02@gmail.com>
2022-05-27 19:28:09 +0300

    Making Driver class

---
+++ Driver.js
@@ -0,0 +1,30 @@
+import { Employee } from "./Employee";
+
+export class Driver extends Employee {
+
+    static numberOfDrivers = 0 // static variable to cont the number of drivers.
+    // Defining the driver properties.
+    #carTypeCanDrive
+
+    // constructor to assign a value to the variables.
+    constructor(name, ID, dateOfBirth, carTypeCanDrive){
+        super(name, ID, dateOfBirth)
+        this.#carTypeCanDrive = carTypeCanDrive
+        Driver.incrementNumberOfDrivers()
+    }
+
+    // getting functions
+    getCTCD() {
+        return this.#carTypeCanDrive
+    }
+
+    // setting functions.
+    setName(newCTCD) {
+        this.#carTypeCanDrive = newCTCD
+    }
+
+    // function to increase the number of drivers.
+    static incrementNumberOfDrivers(){
+        Driver.incrementNumberOfDrivers++
+    }
+}
