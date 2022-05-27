417f54deb36415360837d5f6bc7e4b385cdf94c5
alizmq02 <alizmq02@gmail.com>
2022-05-27 19:32:05 +0300

    editing mistake

--- Driver.js
+++ Driver.js
@@ -1,4 +1,4 @@
-import { Employee } from "./Employee";
+import { Employee } from "./Employee.js";
 
 export class Driver extends Employee {
 
---
+++ Pilot.js
@@ -0,0 +1,5 @@
+import { Employee } from "./Employee.js"
+export class Pilot extends Employee {
+
+    
+}
